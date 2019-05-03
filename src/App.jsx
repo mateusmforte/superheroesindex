14779d8eb9ccf46b871539a163c834d4f248c8fd
Mateus Moura forte <m-forte@hotmail.com>
2019-05-02 21:06:50 -0300

    Componentizando o design do menu principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ListHeroes from './components/ListHeroes';
-import {BrowserRouter,Route} from 'react-router-dom';
+import MainMenu from './components/MainMenu';
+import {BrowserRouter,Route,IndexRoute} from 'react-router-dom';
 function App() {
   return (
     <BrowserRouter>
+      <IndexRoute component={MainMenu}/>
       <Route path="/listheroes" component={ListHeroes}/>
     </BrowserRouter>
   );
